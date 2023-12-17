5c16dc40524341460f230830f771e359506189f4
David <dehong.xu@outlook.com>
2023-12-17 10:34:13 +1000

    Finished home section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,7 +14,26 @@ export default function Home()
                 </h1>
             </div>
             <div className="bot">
-                
+                <div className="bot-title">
+                    <h2>Featured</h2>
+                </div>
+                <div className="bot-content">
+                    <div className="featured-container">
+                        <img src="img/food1.jpg" alt="Featured1"/>
+                        <h3 className="featured-heading">[New] Food Title</h3>
+                        <p className="featured-date">16 Dec 2023</p>
+                    </div>
+                    <div className="featured-container">
+                        <img src="img/food2.jpg" alt="Featured1"/>
+                        <h3 className="featured-heading">[Exclusive] Food Title</h3>
+                        <p className="featured-date">13 Dec 2023</p>
+                    </div>
+                    <div className="featured-container">
+                        <img src="img/food3.jpg" alt="Featured1"/>
+                        <h3 className="featured-heading">[New] Food Title</h3>
+                        <p className="featured-date">10 Dec 2023</p>
+                    </div>
+                </div>                
             </div>
         </div>
     )
